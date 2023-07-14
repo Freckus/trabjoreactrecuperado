f61e3d6fc731180ae93aa9e90a1a2e16e58c24b0
Freckus <ivan.alejandro.mancilla@gmail.com>
2023-07-14 06:25:43 -0400

    tengo solo ahora dos array

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -3,13 +3,24 @@ import Form from "react-bootstrap/Form";
 import React, { useState } from "react";
 import { SubTittle, Dest, Origen } from "./Values";
 import { Col, Container, Row } from "react-bootstrap";
-import { Asientos } from "./Sit";
+//import { Asientos } from "./Sit";
 
 export function getRandomNumber() {
-    return Math.floor(Math.random() * 10000) + 1;
-  }
+  return Math.floor(Math.random() * 10000) + 1;
+}
 
 export function Basic() {
+  var Asientos = [];
+  for (var i = 0; i < 7; i++) {
+    var fila = [];
+    for (var j = 1; j <= 6; j++) {
+      fila.push(j);
+    }
+    Asientos.push({
+      letra: String.fromCharCode(97 + i),
+      numeros: fila,
+    });
+  }
   const [formData, setFormData] = useState({
     id: getRandomNumber(),
     nombre: "",
@@ -17,6 +28,7 @@ export function Basic() {
     edad: "",
     telefono: "",
     rut: "",
+    asiento: "",
   });
 
   const handleChange = (e) => {
@@ -27,6 +39,13 @@ export function Basic() {
     e.preventDefault();
     console.log(formData);
   };
+
+  function handleClick(valor) {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      asiento: valor,
+    }));
+  }
 
   return (
     <Row>
@@ -93,7 +112,31 @@ export function Basic() {
         </Form>
       </Col>
       <Col sm={6} className="border border-info ">
-        <Asientos />
+        <div>
+          <h1 className="text-center">Asientos</h1>
+          {Asientos.map((fila, index) => (
+            <Row key={index}>
+              <Col xs={1} className="text-end">
+                <h3>{fila.letra.toUpperCase()}:</h3>
+              </Col>
+              <Col xs={11}>
+                {fila.numeros.map((numero, numIndex) => (
+                  <Button
+                    key={numIndex}
+                    value={`${fila.letra.toUpperCase()}${numero}`}
+                    title={`${fila.letra.toUpperCase()}${numero}`}
+                    className="mx-1 mb-3"
+                    onClick={() =>
+                      handleClick(`${fila.letra.toUpperCase()}${numero}`)
+                    }
+                  >
+                    {numero}
+                  </Button>
+                ))}
+              </Col>
+            </Row>
+          ))}
+        </div>
       </Col>
     </Row>
   );
@@ -104,7 +147,7 @@ export function SeleDest() {
     id: getRandomNumber(),
     origen: "",
     destino: "",
-    horario: ""
+    horario: "",
   });
   const [origen, setOrigen] = useState("");
   const [destino, setDestino] = useState("");
@@ -208,7 +251,11 @@ export function SeleDest() {
               ))}
             </Form.Select>
             <br />
-            <Button variant="primary" type="submit" onClick={handleAgregarClick}>
+            <Button
+              variant="primary"
+              type="submit"
+              onClick={handleAgregarClick}
+            >
               Agregar
             </Button>
           </Col>
@@ -218,3 +265,5 @@ export function SeleDest() {
     </>
   );
 }
+
+const boleto = () => {};
